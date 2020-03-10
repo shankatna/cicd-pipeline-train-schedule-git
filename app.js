2d7fcb2bf8d5d077bc03c58d208e22eec019fa46
shankatna <sm.katna@gmail.com>
2020-03-10 18:53:55 +0000

    changes done to test git

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var trainsRouter = require('./routes/trains');
 var app = express();
 
 // view engine setup
+// I'm testing the status of git command via this application
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
